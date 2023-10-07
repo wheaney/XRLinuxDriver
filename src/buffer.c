6da2f31c04200cad60cb386fb2b52d12a7f9ef15
wheaney <42350981+wheaney@users.noreply.github.com>
2023-10-07 13:45:16 -0700

    Split out buffering into something reusable, add initial look-ahead support, clean up IPC and add debugging

---
+++ src/buffer.c
@@ -0,0 +1,39 @@
+#include "buffer.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+buffer_type *create_buffer(int size) {
+    buffer_type *buffer = malloc(sizeof(buffer_type));
+    if (buffer == NULL) {
+        return NULL;
+    }
+
+    buffer->size = size;
+    buffer->values = malloc(size * sizeof(float));
+    if (buffer->values == NULL) {
+        free(buffer);
+        return NULL;
+    }
+    buffer->index = 0;
+    buffer->count = 0;
+
+    return buffer;
+}
+
+bool is_full(buffer_type *buffer) {
+    return buffer->count == buffer->size;
+}
+
+float push(buffer_type *buffer, float next_value) {
+    float popped_value = 0;
+    if (is_full(buffer)) {
+        popped_value = buffer->values[buffer->index];
+    } else {
+        buffer->count++;
+    }
+    buffer->values[buffer->index++] = next_value;
+    buffer->index %= buffer->size;
+
+    return popped_value;
+}
