cec6e303d824df8c07dcb9e7f4f5d15cd341d0ee
wheaney <42350981+wheaney@users.noreply.github.com>
2023-10-16 22:02:03 -0700

    Send the remaining IPC values, move XREAL-specific device settings to its own header file, switch to forked module to get new fusion convention for quaternion usage

---
+++ include/xreal.h
@@ -0,0 +1,8 @@
+static const device_properties_type xreal_air_properties = {
+    .resolution_w           = 1920,
+    .resolution_h           = 1080,
+    .fov                    = 46.0,
+
+    // ~6 inches the center of rotation, vs display distance ~12 feet
+    .lens_distance_ratio    = 0.04
+};
