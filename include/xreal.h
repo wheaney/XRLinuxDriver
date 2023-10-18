d79fde9c0c30cad1ed869a3582199c1551cb3084
wheaney <42350981+wheaney@users.noreply.github.com>
2023-10-17 21:30:01 -0700

    Working calibration wait logic, and reset with multi-tap

--- include/xreal.h
+++ include/xreal.h
@@ -4,5 +4,7 @@ static const device_properties_type xreal_air_properties = {
     .fov                    = 46.0,
 
     // ~6 inches the center of rotation, vs display distance ~12 feet
-    .lens_distance_ratio    = 0.04
+    .lens_distance_ratio    = 0.04,
+
+    .calibration_wait_s     = 15
 };
