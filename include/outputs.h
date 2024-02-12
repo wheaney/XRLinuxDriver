8da21ba53525497c9d93da6b112b2b3be8e86e42
David Thomson <jdavidthomson@gmail.com>
2024-02-11 19:53:24 -0500

    forgot to add outputs.h header file for the initial mouse left button single and double click support

--- include/outputs.h
+++ include/outputs.h
@@ -12,5 +12,9 @@ void deinit_outputs();
 // return the rate-of-change of the euler value against the previous euler value, in degrees/sec
 imu_euler_type get_euler_velocities(imu_euler_type euler);
 
+void singletap(uint32_t timestamp_ms, imu_quat_type quat, imu_euler_type velocities, bool ipc_enabled,
+                       bool imu_calibrated, ipc_values_type *ipc_values);
+void doubletap(uint32_t timestamp_ms, imu_quat_type quat, imu_euler_type velocities, bool ipc_enabled,
+                       bool imu_calibrated, ipc_values_type *ipc_values);
 void handle_imu_update(uint32_t timestamp_ms, imu_quat_type quat, imu_euler_type velocities, bool ipc_enabled,
                        bool imu_calibrated, ipc_values_type *ipc_values);
