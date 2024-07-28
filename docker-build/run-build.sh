7afbc258e71a7efde015394820c60c24cc785773
wheaney <42350981+wheaney@users.noreply.github.com>
2024-07-28 09:06:00 -0700

    Update setup messaging

--- docker-build/run-build.sh
+++ docker-build/run-build.sh
@@ -7,11 +7,16 @@ GROUP=$(id -gn $USER)
 
 # Run containers for each architecture
 if [[ "$1" == "x86_64" || -z "$1" ]]; then
+    sudo rm -rf build/
     docker run --rm -t -v ./:/source --platform linux/amd64 -e UA_API_SECRET -e UA_API_SECRET_INTENTIONALLY_EMPTY -e BREEZY_DESKTOP "xr-driver:amd64"
     sudo chown -R $USER:$GROUP out/
 fi
 
 if [[ "$1" == "aarch64" || -z "$1"  ]]; then
+    sudo rm -rf build/
     docker run --rm -t -v ./:/source --platform linux/arm64 -e UA_API_SECRET -e UA_API_SECRET_INTENTIONALLY_EMPTY -e BREEZY_DESKTOP "xr-driver:arm64"
     sudo chown -R $USER:$GROUP out/
 fi
+
+# build directory structure is all owned by root because of docker, delete it all now
+sudo rm -rf build/
