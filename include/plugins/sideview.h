972d294be0168dfa3ea39c9ad9c0105557244598
wheaney <42350981+wheaney@users.noreply.github.com>
2026-07-31 12:26:07 -0700

    Fix sideview position count

--- include/plugins/sideview.h
+++ include/plugins/sideview.h
@@ -2,7 +2,7 @@
 
 #include "plugins.h"
 
-#define SIDEVIEW_POSITION_COUNT 5
+#define SIDEVIEW_POSITION_COUNT 9
 extern const char *sideview_position_names[SIDEVIEW_POSITION_COUNT];
 
 struct sideview_ipc_values_t {
